4446f072cbbf16c3eebbd12dcc49e87489470ee8
Sanghyuk Jung <benelog@gmail.com>
2020-08-08 00:00:33 +0900

    2장 예제 보강

--- ch2-log-batch/src/test/java/kr/co/wikibook/batch/logbatch/CheckDiskSpaceTaskTest.java
+++ ch2-log-batch/src/test/java/kr/co/wikibook/batch/logbatch/CheckDiskSpaceTaskTest.java
@@ -2,13 +2,22 @@ package kr.co.wikibook.batch.logbatch;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CheckDiskSpaceTaskTest {
 
+  CheckDiskSpaceTask task = new CheckDiskSpaceTask();
+
+  @DisplayName("디스크 용량이 기대치보다 많다")
   @Test
-  void checkDiskSpace() {
-    var task = new CheckDiskSpaceTask();
+  void checkDiskSpaceWhenSufficient() {
+    task.run("/", "1");
+  }
+
+  @DisplayName("디스크 용량이 기대치보다 적다")
+  @Test
+  void checkDiskSpaceWhenInsufficient() {
     assertThatThrownBy(() ->
         task.run("/", "100")
     ).isInstanceOf(IllegalStateException.class);
