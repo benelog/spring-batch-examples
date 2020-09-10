201e9eb1b0bc21c229a1d0a01b0f8670828c124b
Sanghyuk Jung <benelog@gmail.com>
2020-09-11 07:44:25 +0900

    테스트 보강

--- ch3-log-batch/src/test/java/kr/co/wikibook/batch/logbatch/AccessLogJobTest.java
+++ ch3-log-batch/src/test/java/kr/co/wikibook/batch/logbatch/AccessLogJobTest.java
@@ -2,7 +2,9 @@ package kr.co.wikibook.batch.logbatch;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
 import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,9 +14,17 @@ import org.springframework.test.jdbc.JdbcTestUtils;
 @SpringBootTest("access-log=classpath:/sample-access-log.csv")
 public class AccessLogJobTest {
 
+  static File output = new File("user-access-summary.csv");
+
+  @BeforeAll
+  static void deleteOutput() {
+    output.delete();
+  }
+
   @Test
   void launchJob(@Autowired DataSource dataSource) {
     int count = JdbcTestUtils.countRowsInTable(new JdbcTemplate(dataSource), "access_log");
     assertThat(count).isGreaterThan(0);
+    assertThat(output.exists()).isTrue();
   }
 }
