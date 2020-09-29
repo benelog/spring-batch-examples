3acd7a7c1315faa0a38179c027042949ac5cf3e7
Sanghyuk Jung <benelog@gmail.com>
2020-09-29 13:36:47 +0900

    로거사용

--- ch4-log-batch/src/main/java/kr/co/wikibook/batch/logbatch/HelloTask.java
+++ ch4-log-batch/src/main/java/kr/co/wikibook/batch/logbatch/HelloTask.java
@@ -1,17 +1,20 @@
 package kr.co.wikibook.batch.logbatch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
 public class HelloTask implements Tasklet {
+  private final Logger log = LoggerFactory.getLogger(HelloTask.class);
   private int repetitions = 0;
 
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
     if (repetitions < 10) {
-      System.out.println("Hello Batch : " + repetitions);
+      log.info("Hello Batch : {}", repetitions);
       repetitions++;
       return RepeatStatus.CONTINUABLE;
     }
