c781ab83e0dbdb6e8f8720db3423918aa5d3bf87
Sanghyuk Jung <benelog@gmail.com>
2020-10-05 13:22:43 +0900

    HelloTask와 RepeatTask 분리

--- ch4-log-batch/src/main/java/kr/co/wikibook/batch/logbatch/RepeatTask.java
+++ ch4-log-batch/src/main/java/kr/co/wikibook/batch/logbatch/RepeatTask.java
@@ -2,18 +2,24 @@ package kr.co.wikibook.batch.logbatch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
-public class HelloTask implements Tasklet {
+public class RepeatTask implements Tasklet {
 
-  private final Logger log = LoggerFactory.getLogger(HelloTask.class);
+  private final Logger log = LoggerFactory.getLogger(RepeatTask.class);
+  private int repetitions = 0;
 
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
-    log.info("Hello Batch");
+    if (repetitions < 10) {
+      log.info("Hello Batch : {}", repetitions);
+      repetitions++;
+      return RepeatStatus.CONTINUABLE;
+    }
     return RepeatStatus.FINISHED;
   }
 }
