64d1fb3270af1fcb53f1064298ab5a6e2f6e68e3
Sanghyuk Jung <benelog@gmail.com>
2021-02-01 07:24:34 +0900

    3장 예제 보강

--- ch3-log-batch/src/main/java/kr/co/wikibook/batch/logbatch/AccessLogCsvReader.java
+++ ch3-log-batch/src/main/java/kr/co/wikibook/batch/logbatch/AccessLogCsvReader.java
@@ -1,34 +1,29 @@
 package kr.co.wikibook.batch.logbatch;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Path;
 
 import org.springframework.core.io.Resource;
 import org.springframework.lang.Nullable;
 
 public class AccessLogCsvReader {
 
-  private final AccessLogLineMapper lineMapper = new AccessLogLineMapper(); // <2>
+  private final AccessLogLineMapper lineMapper = new AccessLogLineMapper();
   private final Resource resource;
-  private BufferedReader bufferedReader; // <1>
+  private BufferedReader bufferedReader;
 
-  public AccessLogCsvReader(Resource resource) { // <3>
+  public AccessLogCsvReader(Resource resource) {
     this.resource = resource;
   }
 
-  public void open() throws IOException { // <4>
+  public void open() throws IOException {
     InputStream inputStream = resource.getInputStream();
     this.bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-    bufferedReader = new BufferedReader(new FileReader(inputFile));
-
   }
 
-  @Nullable // <5>
+  @Nullable
   public AccessLog read() throws IOException {
     String line = this.bufferedReader.readLine();
     if (line == null) {
@@ -37,27 +32,7 @@ public class AccessLogCsvReader {
     return this.lineMapper.mapLine(line);
   }
 
-  public void close() throws IOException { // <6>
+  public void close() throws IOException {
     this.bufferedReader.close();
   }
-
-  private boolean isCsvFile(Path path)  {
-    File inputFile = path.toFile();
-    try ( var lineReader = new BufferedReader(new FileReader(inputFile)) ) {
-
-      String line = null;
-
-      while ((line = lineReader.readLine()) != null) {
-        if (!line.contains(",")) {
-          return false;
-        }
-        if (!StringUtils.isAsciiPrintable(row)) {
-          return false;
-        }
-      }
-    }
-
-    return true;
-  }
-
 }
